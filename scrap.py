f8d4f54e2e6e742d7a52ebfff1595b1d5fd16147
DrZuse <kirillshelest@gmail.com>
2023-03-14 19:42:14 +0200

    scrap

--- scrap.py
+++ scrap.py
@@ -1,1 +1,17 @@
-import beau
+import requests
+from bs4 import BeautifulSoup
+
+URL = 'https://web3.career/defi-jobs'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+jobs = soup.findAll("tr", {"class": "table_row"})
+
+print(jobs)
+
+for job in jobs:
+    print(job.find('h2', {'class': 'my-primary'}).text)
+    badges = job.findAll('a', {'class': 'text-shadow-1px'})
+    for badge in badges:
+        print(badge.text)
